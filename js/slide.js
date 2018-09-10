1f3953fe0ebed2df3db7c07d68518bdca484c030
unadesuga <unauna@YOSUKEnoMacBook-puro.local>
2018-09-10 09:59:35 +0700

    first-commit

---
+++ js/slide.js
@@ -0,0 +1,13 @@
+// slide-------------------------------------------------
+
+
+$(document).ready(function(){
+  $('.slide-photo').slick({
+      // fade: false,
+      // slidesToShow: 4,
+      // vertical: false,
+      // arrows: true
+      dots: true,
+      slidesToShow: 3,
+  });
+});
---
+++ js/vegasu.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.hero-li').vegas({
+    slides: [
+     { src: 'img/done-juniperphoton-776415-unsplash.jpg' },
+     { src: 'img/done-jash-chhabria-658498-unsplash.jpg' },
+   ],
+   timer: true,
+   delay: 6000,
+  });
+ });
